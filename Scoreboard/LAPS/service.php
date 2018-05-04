082e01e31a633b13f22b604e5eee0d507f978b67
Mustafa Ghadiyali <ghadiyalimustafa@gmail.com>
2018-05-04 18:06:35 -0400

    Added to service.php

---
+++ Scoreboard/LAPS/service.php
@@ -0,0 +1,20 @@
+<?php
+	include_once("common.php");
+	$op=$_GET["op"];
+	if($op=="login") {
+		$user=$_GET["uname"];
+		$pass=$_GET["pwd"];
+		$result = login($uname,$pwd);
+		echo $result;
+	}
+	else if($op=="submitCurrentAndGetNextQ") {
+		$uname=$_GET["uname"];	
+		$grade=$_GET["grade"];
+		$result = submitAndGetNext($uname);
+		echo $result;
+	}	
+	else {
+		echo "unsupported op: $op";
+	}
+		
+?>
