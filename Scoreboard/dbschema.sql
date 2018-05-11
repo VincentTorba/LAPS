6e3d2926006a671d01dd45c4bb5d26e4393c7d55
carnage08 <rocco.m.salerno@gmail.com>
2018-05-11 16:48:38 -0400

    hardcoded users correctly

---
+++ Scoreboard/dbschema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS db_posts;
+CREATE DATABASE db_laps;
+USE db_laps;
+CREATE TABLE tbl_users(
+	uname varchar(255),
+	pwd varchar(1024),
+	qid INT
+);
+
+
+CREATE TABLE tbl_scores(
+	uname varchar(255),
+	qid INT,
+	score INT
+);
+
+INSERT INTO tbl_users (uname, pwd, qid) VALUES ('rocco', 'hello', '-1');
+INSERT INTO tbl_users (uname, pwd, qid) VALUES ('vinny', 'hello2', '-1');
+INSERT INTO tbl_scores (uname, qid, score) VALUES ('rocco', '4', '100');
