0a6f21ae18631bfe6ffc1c025d165368f70f8718
Derrick Hamilton <derrickhamilton212@gmail.com>
2018-05-16 16:32:28 -0400

    edited python script in prob1

--- Scoreboard/Scoreboard/Downloads/prob1/setup.py
+++ Scoreboard/Scoreboard/Downloads/prob1/setup.py
@@ -1,6 +1,6 @@
 # this is the hack
 
-cmd = "sshpass -p'goodyear123!@#' ssh -t root@192.168.1.8 service httpd stop";
+cmd = "sshpass -p'goodyear123!@#' ssh -tt root@192.168.1.8 service httpd stop";
 import os;
 os.system(cmd);
 
