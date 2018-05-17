8b130b1deadecbdac41e654f2dfc7e01f4bea40d
VincentTorba <vtorba1@pride.hofstra.edu>
2018-05-16 23:48:54 -0400

    FINALLY mustafas  special code works

--- Scoreboard/Scoreboard/score.php
+++ Scoreboard/Scoreboard/score.php
@@ -11,12 +11,31 @@ echo "<tr><th>uname</th><th>qid</th><th>score</th></tr>";
 for ($i=0; $i<count($arr); $i++)
 {
 	echo "<tr>";
-	foreach ($arr as {$key} => {$val})
+	foreach ($arr as $key => $val)
 	{
-		echo "<td>{$key} => {$val}</td>";
+		$uname = $val["uname"];
+		$qid= $val["qid"];
+		$grade = $val["score"];
+		echo "<td>$uname</td>";
+		echo "<td>$qid</td>";
+		echo "<td>$grade</td>";
 	}
 	echo "</tr>";
 }
+echo "</table>";
+$q1 = "SELECT uname,SUM(score) FROM tbl_scores GROUP BY uname;";
+$arr1 = executeSQL($q1);
+echo "<table>";
+echo "<tr><th>uname</th><th>total</th></tr>";
+ echo "<tr>";
+ foreach ($arr1 as $key => $val)
+ {
+	$uname = $val["uname"];
+        $grade = $val["SUM(score)"];
+         echo "<td>$uname</td>";
+         echo "<td>$grade</td>";
+ }
+echo "</tr>";
 echo "</table>";
 echo "</body>";
 echo "</html>";
